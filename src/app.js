5ca7f07d82d6b57f264cda2ef76790cd5f3fb661
Geovanny Risco <geovannyalexanderr@gmail.com>
2022-06-08 02:41:04 +0200

    Copy code and data from `Choropleth` tutorial
    Ref: https://observablehq.com/@d3/choropleth

--- src/app.js
+++ src/app.js
@@ -1,5 +1,24 @@
+import unemployment from '../data/unemployment.json' assert { type: "json" };
+import us from '../data/us.json' assert { type: "json" };
+import { Choropleth } from './choropleth.js';
+
+const counties = topojson.feature(us, us.objects.counties);
+const states = topojson.feature(us, us.objects.states);
+const statemap = new Map(states.features.map(d => [d.id, d]));
+const statemesh = topojson.mesh(us, us.objects.states, (a, b) => a !== b);
+
+const chart = Choropleth(unemployment, {
+    id: d => d.id,
+    value: d => d.rate,
+    scale: d3.scaleQuantize,
+    domain: [1, 10],
+    range: d3.schemeBlues[9],
+    title: (f, d) => `${f.properties.name}, ${statemap.get(f.id.slice(0, 2)).properties.name}\n${d?.rate}%`,
+    features: counties,
+    borders: statemesh,
+    width: 975,
+    height: 610
+})
 
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>Hello there!</h1>`;
-
-d3.select("h1").style("color", "red");
+appDiv.appendChild(chart);
