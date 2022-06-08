8b4d2f5a933cfb220bf6972c89f1f92e78bba3f2
Geovanny Risco <geovannyalexanderr@gmail.com>
2022-06-08 17:38:21 +0200

    Follow and reproduce offical map tutorial
    https://leafletjs.com/examples/choropleth/

--- src/app.js
+++ src/app.js
@@ -1,24 +1,114 @@
-import unemployment from '../data/unemployment.json' assert { type: "json" };
-import us from '../data/us.json' assert { type: "json" };
-import { Choropleth } from './choropleth.js';
+import statesData from '../data/us-states.json' assert { type: "json" };
 
-const counties = topojson.feature(us, us.objects.counties);
-const states = topojson.feature(us, us.objects.states);
-const statemap = new Map(states.features.map(d => [d.id, d]));
-const statemesh = topojson.mesh(us, us.objects.states, (a, b) => a !== b);
+const map = L.map('map').setView([37.8, -96], 4);
 
-const chart = Choropleth(unemployment, {
-    id: d => d.id,
-    value: d => d.rate,
-    scale: d3.scaleQuantize,
-    domain: [1, 10],
-    range: d3.schemeBlues[9],
-    title: (f, d) => `${f.properties.name}, ${statemap.get(f.id.slice(0, 2)).properties.name}\n${d?.rate}%`,
-    features: counties,
-    borders: statemesh,
-    width: 975,
-    height: 610
-})
+const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
-const appDiv = document.getElementById('app');
-appDiv.appendChild(chart);
+// control that shows state info on hover
+const info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info');
+    this.update();
+    return this._div;
+};
+
+info.update = function (props) {
+    this._div.innerHTML = '<h4>US Population Density</h4>' + (props ?
+        '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>' : 'Hover over a state');
+};
+
+info.addTo(map);
+
+
+// get color depending on population density value
+function getColor(d) {
+    return d > 1000 ? '#800026' :
+        d > 500 ? '#BD0026' :
+            d > 200 ? '#E31A1C' :
+                d > 100 ? '#FC4E2A' :
+                    d > 50 ? '#FD8D3C' :
+                        d > 20 ? '#FEB24C' :
+                            d > 10 ? '#FED976' : '#FFEDA0';
+}
+
+function style(feature) {
+    return {
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7,
+        fillColor: getColor(feature.properties.density)
+    };
+}
+
+function highlightFeature(e) {
+    const layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+
+    info.update(layer.feature.properties);
+}
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+    info.update();
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+/* global statesData */
+const geojson = L.geoJson(statesData, {
+    style: style,
+    onEachFeature: onEachFeature
+}).addTo(map);
+
+map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
+
+
+const legend = L.control({ position: 'bottomright' });
+
+legend.onAdd = function (map) {
+
+    const div = L.DomUtil.create('div', 'info legend');
+    const grades = [0, 10, 20, 50, 100, 200, 500, 1000];
+    const labels = [];
+    let from, to;
+
+    for (let i = 0; i < grades.length; i++) {
+        from = grades[i];
+        to = grades[i + 1];
+
+        labels.push(
+            '<i style="background:' + getColor(from + 1) + '"></i> ' +
+            from + (to ? '&ndash;' + to : '+'));
+    }
+
+    div.innerHTML = labels.join('<br>');
+    return div;
+};
+
+legend.addTo(map);
