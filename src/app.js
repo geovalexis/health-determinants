d26718f446869e95f5f9d3e563376bf8e184b810
Geovanny Risco <geovannyalexanderr@gmail.com>
2022-06-09 17:17:39 +0200

    Abstract all extra data info from to json file

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 import europeData from '../data/europe.json' assert { type: "json" };
 import healthData from '../data/health_data-cleaned.json' assert { type: "json" };
+import healthDataMetaData from '../data/health_data-cleaned-metadata.json' assert { type: "json" };
 import { addInteractiveChoroplethMap } from './choropleth.js';
 
+
 const year = 2016;
-const propertyKey = "healthData";
-const featureKey = "FOODTFAT";
-const featureDescription = "Calorías totales";
-const featureUnits = "Kcal";
+const featureKey = "MNTDISORDSHR";
+
+const featureDescription = healthDataMetaData.find(d => d.key === featureKey).description;
+const featureUnits = healthDataMetaData.find(d => d.key === featureKey).units;
 const data4Year = healthData.filter(d => d.YEAR == year);
 const legendColors = ['#FEB24C', '#FD8D3C', '#FC4E2A', '#E31A1C', '#BD0026', '#800026'];
 const minValue = Math.min(...data4Year.map(item => item[featureKey]));
 const maxValue = Math.max(...data4Year.map(item => item[featureKey]));
 const colorsSpan = (maxValue - minValue) / legendColors.length;
+const propertyKey = "healthData";
 
 function mergeDataIntoGeoJSON(geoJSON, data) {
     for (let i = 0; i < geoJSON.features.length; i++) {
